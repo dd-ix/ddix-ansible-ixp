20b41c596597bb887e74cd938b3317c9668de22a
Thomas Liske <thomas@fiasko-nw.net>
2024-02-23 22:56:58 +0100

    build: fetch peers from ixp-manager database to enable md5 secret support

---
+++ ixp-manager_peers.sql
@@ -0,0 +1,43 @@
+-- This query create a view  on the IXP-Manager tables containing
+-- all the required peer data to build:
+-- - arouteserver's clients.yml (including md5 secrets)
+-- - switchport configs including l2 address filters
+
+CREATE OR REPLACE VIEW `__DDIX__peers` AS
+SELECT `cu`.`name` AS `custname`,
+  `cu`.`autsys` AS `asn`,
+  `pi`.`status` AS `status`,
+  `ixp_manager`.`vlan`.`id` AS `vlanid`,
+  `l2address`.`mac` AS `l2address`,
+  `cu`.`maxprefixes` AS `maxprefixes`,
+  `vli`.`ipv4enabled` AS `ipv4enabled`,
+  `cu`.`peeringmacro` AS `ipv4peeringmacro`,
+  `vli`.`ipv4hostname` AS `ipv4hostname`,
+  `vli`.`ipv4bgpmd5secret` AS `ipv4bgpmd5secret`,
+  `v4`.`address` AS `ipv4address`,
+  `vli`.`ipv6enabled` AS `ipv6enabled`,
+  `cu`.`peeringmacrov6` AS `ipv6peeringmacro`,
+  `vli`.`ipv6hostname` AS `ipv6hostname`,
+  `vli`.`ipv6bgpmd5secret` AS `ipv6bgpmd5secret`,
+  `vli`.`rsclient` AS `rsclient`,
+  `v6`.`address` AS `ipv6address`
+FROM (
+    (
+      `physicalinterface` `pi`
+      JOIN `virtualinterface` `vi`
+    )
+    JOIN (
+      (
+        (
+          `vlaninterface` `vli`
+          LEFT JOIN `ipv4address` `v4` ON(`vli`.`ipv4addressid` = `v4`.`id`)
+        )
+        LEFT JOIN `ipv6address` `v6` ON(`vli`.`ipv6addressid` = `v6`.`id`)
+      )
+      LEFT JOIN `vlan` on(`vli`.`vlanid` = `ixp_manager`.`vlan`.`id`)
+      LEFT JOIN `l2address` ON(`l2address`.`vlan_interface_id` = `vli`.`id`)
+    )
+    JOIN `cust` `cu` ON(`cu`.`id` = `vi`.`custid`)
+  )
+WHERE `pi`.`virtualinterfaceid` = `vi`.`id`
+  AND `vli`.`virtualinterfaceid` = `vi`.`id`
