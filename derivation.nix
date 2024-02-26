fa1be05ce72963cc2a486ad6d4cddf10165d9c53
Marcel <me@m4rc3l.de>
2024-02-26 18:40:21 +0100

    Added ansible general community collection

--- derivation.nix
+++ derivation.nix
@@ -1,19 +1,34 @@
 { lib, pkgs, arouteserver, ... }:
 let
-  self = lib.cleanSource ./.;
   python3 = pkgs.python3.withPackages (pp: with pp; [ matrix-client mysqlclient ]);
+  ansible-general = pkgs.fetchFromGitHub {
+    owner = "ansible-collections";
+    repo = "community.general";
+    rev = "8.3.0";
+    hash = "sha256-Cv9Whp7JEVtkZ33PlkC2+kqkFMvaZq0zAeXqS/48cgY=";
+  };
+  ddix-ansible-ixp = pkgs.stdenv.mkDerivation {
+    name = "libfoo-1.2.3";
+    src = lib.cleanSource ./.;
+    installPhase = ''
+      mkdir -p $out
+      mv ./* $out
+      mkdir -p $out/collections/ansible_collections/community
+      ln -s ${ansible-general} $out/collections/ansible_collections/community/general
+    '';
+  };
 in
 pkgs.writeShellApplication {
   name = "ddix-ansible-ixp";
 
-  runtimeInputs = with pkgs;[
+  runtimeInputs = with pkgs; [
     arouteserver
     bgpq4
   ];
 
   text = ''
     export PYTHONPATH="${python3}/${python3.sitePackages}"
-    cd ${self}/plays
+    cd ${ddix-ansible-ixp}/plays
     exec ${pkgs.ansible}/bin/ansible-playbook ixp.yml "''$@" 
   '';
 }
